ca2858a83b8d7028ea264de6a952aa444a333abd
Amit Gaikwad <amitgaikwad795@gmail.com>
2021-02-28 19:11:19 +0530

    add schema validation using middleware

--- src/routes/v1/user.route.js
+++ src/routes/v1/user.route.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const authController = require('../../controllers/auth.controller');
+const validate = require('../../middlewares/validate');
+const userValidation = require('../../validations/user.validation');
 
 const router = express.Router();
 
-router.post('/', authController.createUser);
+// router.post('/', authController.createUser);
+router.post('/', validate(userValidation.createUser),authController.createUser);
 
 module.exports = router;
