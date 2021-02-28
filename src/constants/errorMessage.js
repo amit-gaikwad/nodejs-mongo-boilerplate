6659db148ccebf885f307189cc5ac9666bf70f57
Amit Gaikwad <amitgaikwad795@gmail.com>
2021-02-28 17:05:24 +0530

    add model to create mongodb collections

---
+++ src/constants/errorMessage.js
@@ -0,0 +1,7 @@
+//Minimum eight characters, at least one uppercase letter, one lowercase letter, one number and one special character
+const ERROR_MESSAGES = {
+    PASSWORD_VALIDATION_ERROR :'Minimum eight characters, at least one uppercase letter, one lowercase letter, one number and one special character',
+    INVALID_EMAIL :'Invalid email'
+}
+
+module.exports =ERROR_MESSAGES;
---
+++ src/models/user.model.js
@@ -0,0 +1,58 @@
+const mongoose = require('mongoose');
+const validator = require('validator');
+const bcrypt = require('bcryptjs');
+const { roles } = require('../config/roles');
+const ERROR_MESSAGES = require('../constants/errorMessage');
+
+const userSchema = mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+      lowercase: true,
+      validate(value) {
+        if (!validator.isEmail(value)) {
+          throw new Error(ERROR_MESSAGES.INVALID_EMAIL);
+        }
+      },
+      password: {
+        type: String,
+        required: true,
+        trim: true,
+        minlength: 8,
+        validate(value) {
+          if (!value.match(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/)) {
+            throw new Error(ERROR_MESSAGES.PASSWORD_VALIDATION_ERROR);
+          }
+        },
+      },
+      role: {
+        type: String,
+        enum: roles,
+        defualt: 'user',
+      },
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+userSchema.pre('save', async function (next) {
+  const user = this;
+  if (user.isModified('password')) {
+    user.password = await bcrypt.hash(user.password, 8);
+  }
+  next();
+});
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
