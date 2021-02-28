cd4dab4afb871cff64b113b5de33a04483c287f7
Amit Gaikwad <amitgaikwad795@gmail.com>
2021-02-28 15:20:20 +0530

    add environment file and configuration

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 const app = require('./app');
+const { port } = require('./config/config');
 
-const port = 8080;
+//const port = 8080;
 app.listen(port, () => {
   console.log(`Listening to port ${port}`);
 });
