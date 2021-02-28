b1f25f1d2ccd55113eacc2e8295347e94d3c14fe
Amit Gaikwad <amitgaikwad795@gmail.com>
2021-02-28 16:15:27 +0530

    add pm2 setup for production run

--- src/config/logger.js
+++ src/config/logger.js
@@ -5,7 +5,6 @@ const logger = winston.createLogger({
   level: env === 'development' ? 'debug' : 'info',
   transports: [
     new winston.transports.Console({
-      level: 'debug',
       stderrLevels: ['error'],
       format: winston.format.combine(
         env === 'development' ? winston.format.colorize() : winston.format.uncolorize(),
